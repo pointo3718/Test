3b471d69c3ee6e949e87df141309ca7cdb18add3
Kangjinwook <98442274+pointo3718@users.noreply.github.com>
2023-02-06 16:07:27 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("vscode java 환경설정 및 github 업로드 테스트");
+    }
+}
